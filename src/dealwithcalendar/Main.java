aab58e4b6404a80632ffb4d8a40bb31686e9d72c
JarLoz <jarloz.kimmo@gmail.com>
2011-03-18 15:07:41 +0200

    The First Commit, initializing the repo

---
+++ src/dealwithcalendar/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dealwithcalendar;
+
+/**
+ *
+ * @author koheikki
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("DEAL WITH IT NOOB!");
+    }
+
+}
